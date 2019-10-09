4768d9cb17b040b5e578bf8b91305b731b472876
bradygaster <bradyg@microsoft.com>
2019-10-09 00:10:45 -0700

    initial creation of razor template

---
+++ chart-source/aspnetcore-razorapp/src/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace RazorApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IndexModel(ILogger<IndexModel> logger, 
+            IConfiguration configuration)
+        {
+            Logger = logger;
+            Configuration = configuration;
+        }
+
+        private readonly ILogger<IndexModel> Logger;
+        private IConfiguration Configuration { get; }
+        public string Title { get; private set; }
+
+        public void OnGet()
+        {
+            Title = string.IsNullOrEmpty(Configuration["TITLE"]) 
+                ? "Welcome to ASP.NET Core Razor on Kubernetes" 
+                : Configuration["TITLE"];
+        }
+    }
+}
